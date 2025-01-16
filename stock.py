60a159ed3a8517c4d862d9c977631da4f74b2d29
Ajeet Khamesra <ajeet.khamesra1@outlook.com>
2025-01-16 18:54:42 +0530

    WebApp Updates

--- stock.py
+++ stock.py
@@ -15,10 +15,16 @@ ticker_symbol = "MSFT"
 # Data for Ticker Symbol
 ticker_data = yf.Ticker(ticker_symbol)
 
+# Pandas Data Frame
 tickerDf = ticker_data.history(period = '1d' ,start = "2010-1-1", end = "2020-1-25")
 
+st.write("## Close Stock Price")
 st.line_chart(tickerDf.Close)
+
+st.write ("## Volume of Stock")
 st.line_chart(tickerDf.Volume)
+
+st.write("## Open Stock Price")
 st.line_chart(tickerDf.Open)
 
 
