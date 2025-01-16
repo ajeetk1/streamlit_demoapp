f55b4ae1c93be65cec0ad4acd7488a66a01d50ab
Ajeet Khamesra <ajeet.khamesra1@outlook.com>
2025-01-16 18:52:05 +0530

    Updates to the WebApp

--- stock.py
+++ stock.py
@@ -2,13 +2,17 @@ import streamlit as st
 import pandas as pd
 import yfinance as yf
 
-
+# MarkDown Heading 
 st.write("# Stock Price App")
 
+# SubHeading we are taking Closing,volume and Open Prices 
 st.write("### Show the Closing Price and Volume of stock of Microsoft ")
 
+
+# Taking Ticker Symbol
 ticker_symbol = "MSFT"
 
+# Data for Ticker Symbol
 ticker_data = yf.Ticker(ticker_symbol)
 
 tickerDf = ticker_data.history(period = '1d' ,start = "2010-1-1", end = "2020-1-25")
